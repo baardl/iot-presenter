b6c8742070e326e4d352d72ea4ca87b7ae3ebb52
baardl <bard.lind@gmail.com>
2016-10-27 11:12:08 +0200

    Using arrows for control.

--- src/main/resources/static/js/presenter.js
+++ src/main/resources/static/js/presenter.js
@@ -1,17 +1,49 @@
 
 var slideNum = 1;
 var imageElement = document.getElementById("imageElement");
+var maxImages = 2;
+
+document.onkeydown = isArrow;
+
+function start(){
+    slideNum = 1;
+    showSlide(slideNum);
+}
 function next() {
-    var showSlide = slideNum + 1;
-    slideNum = showSlide;
-    var imageSrc =  "images/image" + showSlide + ".jpg";
+    var current = slideNum + 1;
+    if (current <= maxImages){
+        showSlide(current);
+    }
+
+}
+
+function previous() {
+    var current = slideNum - 1;
+    if (current > 0) {
+        showSlide(current);
+    }
+}
+
+function showSlide(slide) {
+    slideNum = slide;
+    var imageSrc = "images/image" + slide + ".jpg";
     console.log("Show image " + imageSrc);
     imageElement.src = imageSrc;
 }
-function previous() {
-    var showSlide = slideNum - 1;
-    slideNum = showSlide;
-    var imageSrc =  "images/image" + showSlide + ".jpg";
-    console.log("Show image " + imageSrc);
-    imageElement.src = imageSrc;
+
+function isArrow(e){
+    e = e || window.event;
+
+    if (e.keyCode == '38') {
+        // up arrow
+    }
+    else if (e.keyCode == '40') {
+        // down arrow
+    }
+    else if (e.keyCode == '37') {
+        previous()
+    }
+    else if (e.keyCode == '39') {
+        next();
+    }
 }
